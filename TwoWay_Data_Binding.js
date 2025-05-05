468a4961ee32ed523f5ff2704811ef7e83e30a1f
SriRamKambhampati <sriramkambhampati2000@gmail.com>
2025-05-05 12:55:16 +0530

    Create TwoWay_Data_Binding.js

---
+++ TwoWay_Data_Binding.js
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html ng-app>
+<head><title>Data Binding</title></head>
+<body>
+  <input type="text" ng-model="name" placeholder="Enter your name">
+  <p>Hello, {{ name }}!</p>
+</body>
+</html>
