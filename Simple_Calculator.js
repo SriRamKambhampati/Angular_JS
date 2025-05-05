942ef687daab9ae64a3891cfa88f5807c2859aa4
SriRamKambhampati <sriramkambhampati2000@gmail.com>
2025-05-05 12:56:13 +0530

    Create Simple_Calculator.js

---
+++ Simple_Calculator.js
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html ng-app>
+<head><title>Calculator</title></head>
+<body>
+  <input type="number" ng-model="a"> +
+  <input type="number" ng-model="b"> =
+  <strong>{{ a + b }}</strong>
+</body>
+</html>
